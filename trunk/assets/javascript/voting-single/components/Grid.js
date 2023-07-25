a1df62f9ea9f686c6efd4202b3bfa4d24f498668
Leonardo <leopiccionia@gmail.com>
2023-07-25 16:03:09 -0300

    Votes grid

--- trunk/assets/javascript/voting-single/components/Grid.js
+++ trunk/assets/javascript/voting-single/components/Grid.js
@@ -1,9 +1,22 @@
+function range (length) {
+	return [...Array(length).keys()]
+}
+
 const Grid = ({ count }) => {
+	const normalizedCount = Math.abs(count)
+	const rows = range(normalizedCount)
+
 	return (
 		<>
-			<div className="option-voting-grid">
-				<p>{count}</p>
-				{/* <p>{Math.sqrt(Math.abs(count))}</p> */}
+			<p className="sr-only">{count}</p>
+			<div className="option-voting-grid" style={{ '--grid-rows': normalizedCount }} aria-hidden="true">
+				{rows.map((i) => (
+					<div className="option-voting-grid__row" key={i}>
+						{rows.map((j) => (
+							<div className="option-voting-grid__cell" key={j}/>
+						))}
+					</div>
+				))}
 			</div>
 		</>
 	)
