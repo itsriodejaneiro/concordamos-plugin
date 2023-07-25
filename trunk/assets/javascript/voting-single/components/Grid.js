932bec03837d79a8f0aa223c588b80664be85640
Leonardo <leopiccionia@gmail.com>
2023-07-25 17:29:17 -0300

    Simplify votes grid

--- trunk/assets/javascript/voting-single/components/Grid.js
+++ trunk/assets/javascript/voting-single/components/Grid.js
@@ -1,21 +1,12 @@
-function range (length) {
-	return [...Array(length).keys()]
-}
-
 const Grid = ({ count }) => {
-	const normalizedCount = Math.abs(count)
-	const rows = range(normalizedCount)
+	const squares = count ** 2
 
 	return (
 		<>
 			<p className="sr-only">{count}</p>
-			<div className="option-voting-grid" style={{ '--grid-rows': normalizedCount }} aria-hidden="true">
-				{rows.map((i) => (
-					<div className="option-voting-grid__row" key={i}>
-						{rows.map((j) => (
-							<div className="option-voting-grid__cell" key={j}/>
-						))}
-					</div>
+			<div className="option-voting-grid" style={{ '--grid-rows': Math.abs(count) }} aria-hidden="true">
+				{Array.from({ length: squares }).map((_, i) => (
+					<div className="option-voting-grid__cell" key={i}/>
 				))}
 			</div>
 		</>
