11f772ea7fe58a478afe6f550a07ddd6c6d6818e
Leonardo <leopiccionia@gmail.com>
2023-08-03 18:10:45 -0300

    Basic HTML structure

--- trunk/templates/template-create-user.php
+++ trunk/templates/template-create-user.php
@@ -1,1 +1,14 @@
-<h1>Create user</h1>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?php the_title() ?></title>
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+    <h1>Create user</h1>
+    <?php wp_footer(); ?>
+</body>
+</html>
