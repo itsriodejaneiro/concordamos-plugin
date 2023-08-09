58d9771990b6219c0676edae60ff643a260581d3
Leonardo <leopiccionia@gmail.com>
2023-08-09 09:38:54 -0300

    Refactor: Render React on strict mode

--- trunk/assets/javascript/voting-page/index.js
+++ trunk/assets/javascript/voting-page/index.js
@@ -1,6 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { StrictMode} from 'react'
+import { render } from 'react-dom'
 
-import { App } from './App';
+import { App } from './App'
 
-ReactDOM.render(<App />, document.getElementById('concordamos-voting-form'));
+const elementRender = document.getElementById('concordamos-voting-form')
+
+render(<StrictMode><App/></StrictMode>, elementRender)
