51abdc949584dbb26b6e9506238abce247382e16
Thiago Monteiro <thiagomonteiro.geo@gmail.com>
2023-08-10 17:51:23 -0300

    estilo mminha conta

--- trunk/assets/javascript/my-account/components/UserSettings.js
+++ trunk/assets/javascript/my-account/components/UserSettings.js
@@ -4,28 +4,32 @@ export default function UserSettings ({ user }) {
 	return (
 		<div className="my-account-settings">
 			<div className="my-account-box">
-				<h2>{__('My data', 'concordamos')}</h2>
-				<div className="my-account-field">
-					<dl>
-						<dt>{__('Name', 'concordamos')}</dt>
-						<dd>{user.name}</dd>
-					</dl>
-					<button type="button">{__('Edit name', 'concordamos')}</button>
-				</div>
-				<div className="my-account-field">
-					<dl>
-						<dt>{__('My e-mail', 'concordamos')}</dt>
-						<dd>{user.email}</dd>
-					</dl>
-					<button type="button">{__('Edit e-mail', 'concordamos')}</button>
+				<div className="container">
+					<h2>{__('My data', 'concordamos')}</h2>
+					<div className="my-account-field">
+						<dl>
+							<dt>{__('Name', 'concordamos')}</dt>
+							<dd>{user.name}</dd>
+						</dl>
+						<button type="button">{__('Edit name', 'concordamos')}</button>
+					</div>
+					<div className="my-account-field">
+						<dl>
+							<dt>{__('My e-mail', 'concordamos')}</dt>
+							<dd>{user.email}</dd>
+						</dl>
+						<button type="button">{__('Edit e-mail', 'concordamos')}</button>
+					</div>
 				</div>
 			</div>
 			<div className="my-account-box">
-				<h2>{__('General settings', 'concordamos')}</h2>
-				<button type="button">{__('Edit password', 'concordamos')}</button>
-				<button type="button">{__('Review our privacy policy', 'concordamos')}</button>
-				<button type="button">{__('Review our terms of use', 'concordamos')}</button>
-				<button type="button delete">{__('Delete account', 'concordamos')}</button>
+				<div className="container">
+					<h2>{__('General settings', 'concordamos')}</h2>
+					<button type="button">{__('Edit password', 'concordamos')}</button>
+					<button type="button">{__('Review our privacy policy', 'concordamos')}</button>
+					<button type="button">{__('Review our terms of use', 'concordamos')}</button>
+					<button type="button delete">{__('Delete account', 'concordamos')}</button>
+				</div>
 			</div>
 		</div>
 	)
