57f6fb039fa2b7fc59533f4cc5f67a7c97e76b8a
Thiago Monteiro <thiagomonteiro.geo@gmail.com>
2023-08-11 00:05:57 -0300

    estilo da pagina minha conta desktop

--- trunk/assets/javascript/my-account/components/UserSettings.js
+++ trunk/assets/javascript/my-account/components/UserSettings.js
@@ -20,24 +20,24 @@ export default function UserSettings ({ user }) {
 							<dt>{__('Name', 'concordamos')}</dt>
 							<dd>{user.name}</dd>
 						</dl>
-						<button type="button">{__('Edit name', 'concordamos')}</button>
+						<button className="edit" type="button">{__('Edit name', 'concordamos')}</button>
 					</div>
 					<div className="my-account-field">
 						<dl>
 							<dt>{__('My e-mail', 'concordamos')}</dt>
 							<dd>{user.email}</dd>
 						</dl>
-						<button type="button">{__('Edit e-mail', 'concordamos')}</button>
+						<button className="edit" type="button">{__('Edit e-mail', 'concordamos')}</button>
 					</div>
 				</div>
 			</div>
 			<div className="my-account-box">
 				<div className="container">
 					<h2>{__('General settings', 'concordamos')}</h2>
-					<button type="button">{__('Edit password', 'concordamos')}</button>
-					<button type="button">{__('Review our privacy policy', 'concordamos')}</button>
-					<button type="button">{__('Review our terms of use', 'concordamos')}</button>
-					<button type="button delete">{__('Delete account', 'concordamos')}</button>
+					<button className="edit" type="button">{__('Edit password', 'concordamos')}</button>
+					<button className="edit" type="button">{__('Review our privacy policy', 'concordamos')}</button>
+					<button className="edit" type="button">{__('Review our terms of use', 'concordamos')}</button>
+					<button className="delete-acount" type="button delete">{__('Delete account', 'concordamos')}</button>
 				</div>
 			</div>
 		</div>
