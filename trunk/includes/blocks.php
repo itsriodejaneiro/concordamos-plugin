56de1a4221a6ba84eba7c99b619f75863f106479
Leonardo <leopiccionia@gmail.com>
2023-08-11 17:04:13 -0300

    Login block bootstrap

--- trunk/includes/blocks.php
+++ trunk/includes/blocks.php
@@ -2,7 +2,20 @@
 
 namespace Concordamos;
 
-function register_blocks () {
+function register_login_block () {
+	wp_register_style( 'concordamos-login-block-style', CONCORDAMOS_PLUGIN_URL . 'build/css/login-block.css', [ 'concordamos-style' ] , CONCORDAMOS_PLUGIN_VERSION );
+	wp_register_script( 'concordamos-login-block-admin', CONCORDAMOS_PLUGIN_URL . 'build/js/login-block/editor.js', [ 'wp-editor', 'wp-element', 'wp-i18n' ] , CONCORDAMOS_PLUGIN_VERSION );
+
+	register_block_type( 'concordamos/login', [
+		'api_version' => 2,
+		'editor_script' => 'concordamos-login-block-admin',
+		'styles' => [ 'concordamos-login-block-style' ],
+		'render_callback' => 'Concordamos\render_login_block',
+	] );
+}
+
+function register_votings_block () {
+	wp_register_style( 'concordamos-votings-block-style', CONCORDAMOS_PLUGIN_URL . 'build/css/votings-block.css', [ 'concordamos-style' ] , CONCORDAMOS_PLUGIN_VERSION );
 	wp_register_script( 'concordamos-votings-block', CONCORDAMOS_PLUGIN_URL . 'build/js/votings-block/index.js', [ 'wp-element', 'wp-i18n' ] , CONCORDAMOS_PLUGIN_VERSION );
 
 	wp_register_script( 'concordamos-votings-block-admin', CONCORDAMOS_PLUGIN_URL . 'build/js/votings-block/editor.js', [ 'wp-editor', 'wp-element', 'wp-i18n' ] , CONCORDAMOS_PLUGIN_VERSION );
@@ -24,4 +37,13 @@ function register_blocks () {
 	] );
 }
 
-add_action( 'init', 'Concordamos\register_blocks' );
+function render_login_block ( $attributes ) {
+	ob_start();
+	include __DIR__ . '/blocks/login.php';
+	$template = ob_get_contents();
+	ob_end_clean();
+	return $template;
+}
+
+add_action( 'init', 'Concordamos\register_login_block' );
+add_action( 'init', 'Concordamos\register_votings_block' );
