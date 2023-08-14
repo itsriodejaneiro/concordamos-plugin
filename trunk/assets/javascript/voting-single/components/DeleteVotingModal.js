7d083859c7894f928267bd3da03815ce22d3ef13
Leonardo <leopiccionia@gmail.com>
2023-08-14 15:31:43 -0300

    Delete voting

---
+++ trunk/assets/javascript/voting-single/components/DeleteVotingModal.js
@@ -0,0 +1,15 @@
+import { __, sprintf } from '@wordpress/i18n'
+
+import Modal from '../../shared/components/Modal'
+
+export default function DeleteVotingModal ({ controller }) {
+	return (
+		<Modal controller={controller} danger={true}>
+			<h2>{__('Do you want to delete voting?', 'concordamos')}</h2>
+			<p dangerouslySetInnerHTML={ { __html: sprintf(__('For deleting this voting, send a message for the platform administration with the subject "Delete voting" at the email <a href="mailto:%s">%s</a>'), 'concordamos@mail.com', 'concordamos@mail.com') } }/>
+			<div className="buttons">
+				<button type="button" className="button primary" onClick={controller.close}>{__('Go back', 'concordamos')}</button>
+			</div>
+		</Modal>
+	)
+}
