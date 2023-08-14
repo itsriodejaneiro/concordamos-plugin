8de5138b52b3a8d369b64fce4d78dcf6ca6186ad
Matheus Gimenez <matheus.gimenez@hacklab.com.br>
2023-08-14 14:17:29 -0300

    registro de usuário finalizado #44 #43

--- trunk/concordamos.php
+++ trunk/concordamos.php
@@ -28,6 +28,7 @@ require_once( 'includes/class/class-metadata.php' );
 require_once( 'includes/class/class-taxonomy.php' );
 require_once( 'includes/class/class-user-metadata.php' );
 require_once( 'includes/class/class-user-login.php' );
+require_once( 'includes/class/class-user-register.php' );
 
 function concordamos_init() {
 	// Add custom role `concordamos_network`
--- trunk/templates/template-create-user.php
+++ trunk/templates/template-create-user.php
@@ -15,56 +15,61 @@ $site_title = get_bloginfo( 'name' );
     
 </head>
 <body <?php body_class(); ?>>
-	<?php wp_body_open(); ?>
+    <?php wp_body_open(); ?>
     <section class="concordamos-balloon">
         <div class="balloon-content">
-            <p class="balloon-subtitle">A way to vote for consensus</p>
-            <img src="<?php echo CONCORDAMOS_PLUGIN_URL . 'assets/images/login-logo.png'; ?>" alt="Logo do Login">
-            <p class="balloon-subtitle with">With <?php echo $site_title ?></p>
-        </div>    
+            <p class="balloon-subtitle"><?php _e('A way to vote for consensus', 'concordamos'); ?></p>
+            <img src="<?php echo CONCORDAMOS_PLUGIN_URL . 'assets/images/login-logo.png'; ?>" alt="<?php _e('Login Logo', 'concordamos'); ?>">
+            <p class="balloon-subtitle with"><?php printf(__('With %s', 'concordamos'), $site_title); ?></p>
+        </div>
     </section>
-    <section class="register-form">
+    <form class="register-form" id="register-form">
         <div class="container">
             <div class="name-field">
                 <label>
-                    <span>Name</span>
+                    <span><?php _e('Name', 'concordamos'); ?></span>
                     <input name="name" type="text">
+                    <span class="errors" id="name-errors"></span>
                 </label>
             </div>
             <div class="email-field">
                 <label>
-                    <span>E-mail</span>
+                    <span><?php _e('E-mail', 'concordamos'); ?></span>
                     <input name="email" type="text">
+                    <span class="errors" id="email-errors"></span>
                 </label>
             </div>
             <div class="register-password-field">
                 <label>
-                    <span>Password</span>
+                    <span><?php _e('Password', 'concordamos'); ?></span>
                     <input name="password" type="text">
+                    <span class="errors" id="password-errors"></span>
                 </label>
                 <label>
-                    <span>Repeat password</span>
+                    <span><?php _e('Repeat password', 'concordamos'); ?></span>
                     <input name="repeat-password" type="text">
+                    <span class="errors" id="repeat-password-errors"></span>
                 </label>
-            </div> 
-            <div class="accept-checkbox-terms">    
-                <input type="checkbox" />
-                <label for="scales">You accept the <a>Privacy Policy</a> and <a>Terms of Use</a></label>
             </div>
-            <div class="accept-checkbox-image">    
-                <input type="checkbox" />
-                <label for="scales">You accept the <a>Image Terms of Use</a></label>
-            </div>    
+            <div class="accept-checkbox-terms">
+                <input type="checkbox" name="terms"/>
+                <label for="scales"><?php _e('You accept the', 'concordamos'); ?> <a><?php _e('Privacy Policy', 'concordamos'); ?></a> <?php _e('and', 'concordamos'); ?> <a><?php _e('Terms of Use', 'concordamos'); ?></a></label>
+            </div>
+            <div class="accept-checkbox-image">
+                <input type="checkbox" name="image-terms"/>
+                <label for="scales"><?php _e('You accept the', 'concordamos'); ?> <a><?php _e('Image Terms of Use', 'concordamos'); ?></a></label>
+            </div>
+            <div id="status"></div>
             <div class="register-button">
-                <button class="register-submit" type="submit">Let's go!</button>
-                <p>Already have an account? <a href="#">Access here</a>.</p>
-                <img src="" alt="">   
+                <button class="register-submit" type="submit"><?php _e("Let's go!", 'concordamos'); ?></button>
+                <p><?php _e('Already have an account?', 'concordamos'); ?> <a href="#"><?php _e('Access here', 'concordamos'); ?></a>.</p>
+                <img src="" alt="">
             </div>
             <div class="its-logo">
-                <img src="<?php echo CONCORDAMOS_PLUGIN_URL . 'assets/images/its-login-logo.png'; ?>" alt="Logo its Login">
+                <img src="<?php echo CONCORDAMOS_PLUGIN_URL . 'assets/images/its-login-logo.png'; ?>" alt="<?php _e('ITS Login Logo', 'concordamos'); ?>">
             </div>
         </div>
-    </section>
+    </form>
     <?php wp_footer(); ?>
 </body>
 </html>
