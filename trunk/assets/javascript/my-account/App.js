d8d21d82e48383326ebfed7b69216f44e2187470
Leonardo <leopiccionia@gmail.com>
2023-08-14 10:31:59 -0300

    Add signout button

--- trunk/assets/javascript/my-account/App.js
+++ trunk/assets/javascript/my-account/App.js
@@ -6,6 +6,7 @@ import ParticipatedVotings from './components/ParticipatedVotings'
 import Tabs from '../shared/components/Tabs'
 import UserSettings from './components/UserSettings'
 import { useFetch } from '../shared/hooks/fetch'
+import { navigateTo } from '../shared/utils/location'
 
 const tabs = [
 	{ id: 'created', label: __('Votings created by me', 'concordamos') },
@@ -27,11 +28,30 @@ export function App ({ initialData }) {
 		return null
 	}
 
+	function handleSignOut (event) {
+		fetch(new URL('logout', concordamos.rest_url)), {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+				'X-WP-Nonce': concordamos.nonce,
+			},
+		}
+		.then(() => {
+			navigateTo('/voting')
+		})
+		.catch((error) => console.error(error))
+	}
+
 	return (
 		<div className="my-account">
 			<div className="my-account-header">
-				<div className="my-account-header__name">{__('My account', 'concordamos')}</div>
-				<h1>{sprintf(__('Hello, %s', 'concordamos'), user.name)}</h1>
+				<div className="my-account-header__wrapper">
+					<div>
+						<div className="my-account-header__name">{__('My account', 'concordamos')}</div>
+						<h1>{sprintf(__('Hello, %s', 'concordamos'), user.name)}</h1>
+					</div>
+					<button type="button" className="button edit" onClick={handleSignOut}>{__('Sign out', 'concordamos')}</button>
+				</div>
 			</div>
 			<UserSettings user={user}/>
 			<div className="my-account-panel">
