8e25f70a25c2b8ce593550a817c00943c3d716be
Leonardo <leopiccionia@gmail.com>
2024-03-04 17:13:22 -0300

    Fix

--- trunk/includes/helpers.php
+++ trunk/includes/helpers.php
@@ -288,8 +288,8 @@ function get_page_by_template( string $template ) {
 function get_post_by_slug( string $post_type, string $slug ) {
 	$posts = get_posts(
 		array(
+			'name' => $slug,
 			'post_type' => $post_type,
-			'post_name' => $slug,
 		)
 	);
 
