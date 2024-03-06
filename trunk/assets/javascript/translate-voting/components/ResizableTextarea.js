cbb1bb026e4fe57d68fc9e745d1c9769fa1b3189
Leonardo <leopiccionia@gmail.com>
2024-03-06 14:17:15 -0300

    Fix edge cases of resizable textarea

--- trunk/assets/javascript/translate-voting/components/ResizableTextarea.js
+++ trunk/assets/javascript/translate-voting/components/ResizableTextarea.js
@@ -1,14 +1,27 @@
+import { useEffect, useRef } from 'react'
+
 const noop = () => {}
 
+function resizeTextarea (el) {
+	el.style.height = 'auto'
+	el.style.height = `${el.scrollHeight + 6}px`
+}
+
 export default function ResizableTextarea ({ onInput = noop, ...attrs }) {
+	const textareaRef = useRef(null)
+
+	useEffect(() => {
+		if (textareaRef.current) {
+			resizeTextarea(textareaRef.current)
+		}
+	}, [textareaRef.current])
+
 	function innerOnInput (event) {
-		const textarea = event.target
-		textarea.style.height = 'auto'
-		textarea.style.height = `${textarea.scrollHeight + 6}px`
+		resizeTextarea(event.target)
 		onInput(event)
 	}
 
 	return (
-		<textarea className="resizable-textarea" onInput={innerOnInput} {...attrs}/>
+		<textarea className="resizable-textarea" ref={textareaRef} onInput={innerOnInput} {...attrs}/>
 	)
 }
