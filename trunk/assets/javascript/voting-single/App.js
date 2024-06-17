b044b885bd6d6a376992704b1864e6bc05d1f677
Leonardo <leopiccionia@gmail.com>
2024-06-17 11:56:20 -0300

    Simplify App.js

--- trunk/assets/javascript/voting-single/App.js
+++ trunk/assets/javascript/voting-single/App.js
@@ -1,15 +1,10 @@
-import Single from "./pages/Single";
-import Panel from "./pages/Panel";
+import Panel from './pages/Panel'
+import Single from './pages/Single'
 
 export function App({ initialData }) {
-	const { credits_voter, date_end, is_panel, options } = initialData;
-	return (
-		<>
-			{
-				is_panel
-				? <Panel initialData={initialData} />
-				: <Single initialData={initialData} />
-			}
-		</>
+	return initialData.is_panel ? (
+		<Panel initialData={initialData} />
+	) : (
+		<Single initialData={initialData} />
 	)
 }
